a3f6b1f8fa9fb2625ff7f1e4a4812ee93f663984
Xi Qi <realxqi@gmail.com>
2018-11-28 01:05:58 +0800

    add spring security

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -3,8 +3,10 @@ package com.dmall.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableFeignClients
 public class OrderServiceApplication {
 
--- src/main/java/com/dmall/orderservice/apis/OrderController.java
+++ src/main/java/com/dmall/orderservice/apis/OrderController.java
@@ -6,6 +6,7 @@ import com.dmall.orderservice.model.Product;
 import com.dmall.orderservice.model.Shipping;
 import com.dmall.orderservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
