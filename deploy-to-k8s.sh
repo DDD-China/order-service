ab3fea8cbb299fa9b486bb9373eea9ac4c1319e2
Junfeng Dai <ryandjf@hotmail.com>
2018-11-29 00:16:29 +0800

    fix build

--- deploy-to-k8s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CIRCLE_BUILD_NUM_DOCKER=0.1.1
-sed "s/BUILD_VERSION/$CIRCLE_BUILD_NUM_DOCKER/g" order-service-kube.yaml > build/order-service-kube.yaml
-kubectl apply -f build/order-service-kube.yaml
